fcb1458223b0b65ade65fedd51848dd5401799d5
Cedric Raguenaud <cedric@raguenaud.earth>
2026-06-12 18:28:24 +0000

    Added licence

--- tests/RoofControl.Tests/Talon6ProtocolTests.cs
+++ tests/RoofControl.Tests/Talon6ProtocolTests.cs
@@ -1,3 +1,20 @@
+// <copyright file="Talon6ProtocolTests.cs" company="">
+// Copyright (c) 2026 Cedric Raguenaud
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+// </copyright>
+
 using RoofControl.Core.Models;
 using RoofControl.Talon6;
 
